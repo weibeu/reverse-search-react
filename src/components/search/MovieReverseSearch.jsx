18fd832e465cb24abf0489663c9ca939cb7a3f69
weibeu <deepakrajko14@gmail.com>
2022-03-24 04:30:13 +0530

    Improve layout and appearance

--- src/components/search/MovieReverseSearch.jsx
+++ src/components/search/MovieReverseSearch.jsx
@@ -35,9 +35,13 @@ export default class MovieReverseSearch extends Component {
                 <span className="image main"><img src={movieDetails.poster} alt={movieDetails.originaltitle} /></span>
                 <h2><span className="icon fa-comment-dots" /> The Dialogues</h2>
                 <blockquote><h4 style={{marginBottom: 0}}>{this.searchQuery}</h4></blockquote>
-                <p style={{textAlign: "center"}}>
-                    {subtitleMeta.map(item => item["is_key_phrase"] ? <><b>{item["dialogue"]}</b><br /></> : <>{item["dialogue"]}<br /></>)}
-                </p>
+                <div style={{textAlign: "center"}}>
+                    {subtitleMeta.map((item, index) => item["is_key_phrase"] 
+                        ? <p key={index} style={{marginBottom: 0}}><b>{item["dialogue"]}</b></p>
+                        : <p key={index} style={{marginBottom: 0}}>{item["dialogue"]}</p>
+                    )}
+                </div>
+                <hr />
                 <h2><span className="icon fa-chart-bar" /> Movie Details</h2>
                 <table>
                     <thead>
@@ -60,32 +64,34 @@ export default class MovieReverseSearch extends Component {
                     </tbody>
                 </table>
                 <h2><span className="icon fa-user" /> Cast Details</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            {/* <th>Birth Year</th> */}
-                            <th>Professions</th>
-                            <th>Role</th>
-                            <th>Character</th>
-                            <th>Appeared In</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {movieDetails.cast.map(item => {
-                            return (
-                                <tr>
-                                    <td>{item["primaryname"]}</td>
-                                    {/* <td>{item["birthyear"]}</td> */}
-                                    <td>{item["primaryprofession"]}</td>
-                                    <td>{item["category"]}</td>
-                                    <td>{item["characters"]}</td>
-                                    <td>{"".concat(item["knownfortitles"])}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
+                <div className="cast-container">
+                    <table className="alt cast-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                {/* <th>Birth Year</th> */}
+                                <th>Professions</th>
+                                <th>Role</th>
+                                <th>Character</th>
+                                <th>Appeared In</th>
+                            </tr>
+                        </thead>
+                        <tbody className="cast-tbody">
+                            {movieDetails.cast.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td><b>{item["primaryname"]}</b></td>
+                                        {/* <td>{item["birthyear"]}</td> */}
+                                        <td>{item["primaryprofession"]}</td>
+                                        <td>{item["category"]}</td>
+                                        <td>{item["characters"]}</td>
+                                        <td>{"".concat(item["knownfortitles"])}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </>
         )
     }
